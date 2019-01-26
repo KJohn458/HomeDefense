b3b1d2ee7ef1942a85c581b747ada7ccc39584c6
Andrew Ward <andrewward22499@gmail.com>
2019-01-26 01:15:48 -0600

    Adds Death Screen
    adds a death screen and a way to return to main menu from death screen

---
+++ Assets/Andrew/Scripts/BacktoMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BacktoMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public void Update(){
+		if (Input.GetMouseButtonDown(0))
+			SceneManager.LoadScene(0);
+	}
+}
+	
