9e33e1c070d90a74555f36231b98142af1715dd9
Andrew Ward <andrewward22499@gmail.com>
2019-01-26 00:07:06 -0600

    Adds Health Script
    adds health and health script

---
+++ Assets/Andrew/Scripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Health : MonoBehaviour
+{
+	public int health;
+	public Text text;
+
+    void Start()
+    {
+        health = 5;
+    }
+
+
+    void Update()
+    {
+        text.text = health.ToString();
+    }
+	
+	public void Damage()
+	{
+		health--;
+	}
+}
