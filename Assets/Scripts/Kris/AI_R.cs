79dbe2e8c2e3377346cf8acabae88e3dece82cd7
Clayton H <cxanderh@gmail.com>
2019-01-27 01:31:52 -0600

    S

--- Assets/Scripts/Kris/AI_R.cs
+++ Assets/Scripts/Kris/AI_R.cs
@@ -59,7 +59,7 @@ public class AI_R : MonoBehaviour
             R.SetBool("isWalking", false);
         }
 
-        if (hasAttacked == false)
+        if (agent.speed == 0)
         {
             R.Play("Attack");
         }
