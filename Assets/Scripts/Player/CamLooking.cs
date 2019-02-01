54e9a03ae72a3afc04c704e7c39278beda9055c9
Justin Liles <Justin.Liles@live.com>
2019-01-31 20:57:09 -0600

    Added InputManager and Player Scripts  into Project

---
+++ Assets/Scripts/Player/CamLooking.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamLooking : MonoBehaviour
+{
+    public float mouseSensitivivty;
+    public float Xmax = 30.0f;
+    public float Xmin = -30.0f;
+    public float speed = 100f;
+    public float maxAngle = 20f;
+
+    GameObject Player;
+    float xAxisClamp = 0.0f;
+    float curAngle = 0f;
+    void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float MouseX = InputManager.instance.Looking().x;
+        float MouseY = InputManager.instance.Looking().y;
+
+        float rotAmountX = MouseX * mouseSensitivivty;
+        float rotAmountY = MouseY * mouseSensitivivty;
+
+        xAxisClamp -= rotAmountY;
+
+        Vector3 targetRotCam = transform.rotation.eulerAngles;
+        Vector3 targetRotBody = Player.transform.rotation.eulerAngles;
+
+        targetRotCam.x -= rotAmountY;
+        targetRotCam.z = 0;
+        targetRotBody.y += rotAmountX;
+        
+        if(xAxisClamp > Xmax)
+        {
+            xAxisClamp = Xmax;
+            targetRotCam.x = Xmax;
+        }
+        else if (xAxisClamp < Xmin)
+        {
+            xAxisClamp = Xmin;
+            targetRotCam.x = Xmin;
+        }
+
+        if (InputManager.instance.Lean_L())
+        {
+            curAngle = Mathf.MoveTowardsAngle(curAngle, maxAngle, speed * Time.deltaTime);
+            transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(-1, 0.8f, 0), 0.1f);
+            //transform.localPosition = new Vector3(-1,0.8f,0);;
+        }
+        else if (InputManager.instance.Lean_R())
+        {
+            curAngle = Mathf.MoveTowardsAngle(curAngle, -maxAngle, speed * Time.deltaTime);
+            transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(1, 0.8f, 0), 0.1f);
+            //transform.localPosition = new Vector3(1,0.8f,0);
+        }
+        else
+        {
+            curAngle = Mathf.MoveTowardsAngle(curAngle, 0f, speed * Time.deltaTime);
+            transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(0, 0.8f, 0), 0.1f);
+            //transform.localPosition = new Vector3(0,0.8f,0);
+        }
+
+        if(InputManager.instance.Lean_L() || InputManager.instance.Lean_R())
+        {
+            transform.localRotation = Quaternion.AngleAxis(curAngle, Vector3.forward); 
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(targetRotCam);
+            Player.transform.rotation = Quaternion.Euler(targetRotBody);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float originalSpeed = 10.0f;
+    public float speed;
+    public float gravity = 20.0f;
+    public float sprintMeater = 100.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    private bool iscrouching = false;
+    private bool isSprinting = false;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        speed = originalSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        if (controller.isGrounded)
+        {
+            Crouching();
+            Sprinting();
+            Movement();
+        }
+
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void Movement()
+    {
+            moveDirection = new Vector3(InputManager.instance.Move().x, 0.0f, InputManager.instance.Move().z);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
+    }
+
+    private void Sprinting()
+    {
+        if (InputManager.instance.Sprint())
+        {
+            isSprinting = true;
+
+            if(sprintMeater <= 100.0f && sprintMeater > 0.0f && !iscrouching)
+            {
+                //speed = originalSpeed * (2.0f * ( sprintMeater * 0.01f ));
+                speed = originalSpeed * 2.0f;
+                sprintMeater -= 0.5f;
+
+                if (speed < 10.0f)
+                {
+                    speed = 10.0f;
+                }
+            }
+            else if (sprintMeater <= 0.0f)
+            {
+                sprintMeater = 0.0f;
+                speed = originalSpeed;
+            }
+        }
+        else
+        {
+            speed = originalSpeed;
+
+            if(sprintMeater >= 100.0f)
+            {
+                sprintMeater = 100.0f;
+            }
+            else
+            {
+                sprintMeater += 0.5f;
+            }
+
+            isSprinting = false;
+        }
+    }
+
+    private void Crouching()
+    {
+        if(!isSprinting)
+        {
+            if(InputManager.instance.Crouch())
+            {
+                iscrouching = true;
+                transform.localScale = new Vector3(1, 0.3f, 1);
+            }
+            else
+            {
+                iscrouching = false;
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+    }
+}
